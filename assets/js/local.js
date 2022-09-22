e911ffb805c7dd9e7cdd57db4441ea13b1ed1e95
danieldjakam <86420544+danieldjakam@users.noreply.github.com>
2022-09-22 19:34:47 +0100

    change local.js file for lang

--- assets/js/local.js
+++ assets/js/local.js
@@ -2,9 +2,11 @@ const worlds = {
     en: {
         'already-do-task': "Already do task",
         'todo-task': 'Todo task',
+        'new-task': 'New task'
     },
     fr: {
         'already-do-task': "Tâches déjà faites",
-        'todo-task': 'Tâches à faire'
+        'todo-task': 'Tâches à faire',
+        'new-task': 'Nouvelle tâche'
     }
 }
