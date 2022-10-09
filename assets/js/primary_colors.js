051e5af1a6ab960505d4b7c8c48f52956e2df388
Daniel <danidjakam@gmail.com>
2022-10-09 01:56:19 +0100

    fix one error for the color change

--- assets/js/primary_colors.js
+++ assets/js/primary_colors.js
@@ -1,4 +1,4 @@
-selesctColors.forEach(selectColor => {
+selectColors.forEach(selectColor => {
     console.log(selectColor);
     selectColor.addEventListener('click', (e) => {
         setSetting('color', e.target.dataset.color);
